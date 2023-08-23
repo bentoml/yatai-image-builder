6faa95b3a4a3d50799c7f0a980658eb2f65edd10
Sauyon Lee <git@sjle.co>
2023-08-23 03:02:32 -0700

    remove default image builder resources

--- controllers/resources/bentorequest_controller.go
+++ controllers/resources/bentorequest_controller.go
@@ -1737,16 +1737,6 @@ echo "Done"
 		TTY:             true,
 		Stdin:           true,
 		SecurityContext: builderContainerSecurityContext,
-		Resources: corev1.ResourceRequirements{
-			Requests: corev1.ResourceList{
-				corev1.ResourceCPU:    resource.MustParse("500m"),
-				corev1.ResourceMemory: resource.MustParse("1Gi"),
-			},
-			Limits: corev1.ResourceList{
-				corev1.ResourceCPU:    resource.MustParse("1"),
-				corev1.ResourceMemory: resource.MustParse("3Gi"),
-			},
-		},
 	}
 
 	if globalDefaultImageBuilderContainerResources != nil {
