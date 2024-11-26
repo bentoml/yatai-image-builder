2337de0481cec4ab2430057376a4791b03ace818
yetone <yetoneful@gmail.com>
2024-11-26 14:06:03 +0800

    fix: s3 client config

--- bento-image-snapshotter/fs/stargzs3/resolvehandler.go
+++ bento-image-snapshotter/fs/stargzs3/resolvehandler.go
@@ -9,16 +9,121 @@ import (
 	"os"
 	"regexp"
 
+	urlpkg "net/url"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go/middleware"
 	"github.com/containerd/log"
 	"github.com/containerd/stargz-snapshotter/fs/remote"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 
+	smithyhttp "github.com/aws/smithy-go/transport/http"
+
 	"github.com/bentoml/yatai-image-builder/common"
 )
+
+var sentinelURL = urlpkg.URL{}
+
+const (
+	// Amazon Accelerated Transfer endpoint
+	transferAccelEndpoint = "s3-accelerate.amazonaws.com"
+
+	// Google Cloud Storage endpoint
+	gcsEndpoint = "storage.googleapis.com"
+)
+
+func supportsTransferAcceleration(endpoint urlpkg.URL) bool {
+	return endpoint.Hostname() == transferAccelEndpoint
+}
+
+func IsGoogleEndpoint(endpoint urlpkg.URL) bool {
+	return endpoint.Hostname() == gcsEndpoint
+}
+
+// isVirtualHostStyle reports whether the given endpoint supports S3 virtual
+// host style bucket name resolving. If a custom S3 API compatible endpoint is
+// given, resolve the bucketname from the URL path.
+func isVirtualHostStyle(endpoint urlpkg.URL) bool {
+	return endpoint == sentinelURL || supportsTransferAcceleration(endpoint) || IsGoogleEndpoint(endpoint)
+}
+
+func parseEndpoint(endpoint string) (urlpkg.URL, error) {
+	if endpoint == "" {
+		return sentinelURL, nil
+	}
+
+	u, err := urlpkg.Parse(endpoint)
+	if err != nil {
+		return sentinelURL, errors.Wrapf(err, "failed to parse endpoint: %s", endpoint)
+	}
+
+	return *u, nil
+}
+
+// ignoreSigningHeaders excludes the listed headers
+// from the request signature because some providers may alter them.
+//
+// See https://github.com/aws/aws-sdk-go-v2/issues/1816.
+func ignoreSigningHeaders(o *s3.Options, headers []string) {
+	o.APIOptions = append(o.APIOptions, func(stack *middleware.Stack) error {
+		if err := stack.Finalize.Insert(ignoreHeaders(headers), "Signing", middleware.Before); err != nil {
+			return errors.Wrap(err, "failed to insert ignoreHeaders middleware")
+		}
+
+		if err := stack.Finalize.Insert(restoreIgnored(), "Signing", middleware.After); err != nil {
+			return errors.Wrap(err, "failed to insert restoreIgnored middleware")
+		}
+
+		return nil
+	})
+}
+
+type ignoredHeadersKey struct{}
+
+func ignoreHeaders(headers []string) middleware.FinalizeMiddleware {
+	return middleware.FinalizeMiddlewareFunc(
+		"IgnoreHeaders",
+		func(ctx context.Context, in middleware.FinalizeInput, next middleware.FinalizeHandler) (out middleware.FinalizeOutput, metadata middleware.Metadata, err error) {
+			req, ok := in.Request.(*smithyhttp.Request)
+			if !ok {
+				return out, metadata, &v4.SigningError{Err: fmt.Errorf("(ignoreHeaders) unexpected request middleware type %T", in.Request)}
+			}
+
+			ignored := make(map[string]string, len(headers))
+			for _, h := range headers {
+				ignored[h] = req.Header.Get(h)
+				req.Header.Del(h)
+			}
+
+			ctx = middleware.WithStackValue(ctx, ignoredHeadersKey{}, ignored)
+
+			return next.HandleFinalize(ctx, in)
+		},
+	)
+}
+
+func restoreIgnored() middleware.FinalizeMiddleware {
+	return middleware.FinalizeMiddlewareFunc(
+		"RestoreIgnored",
+		func(ctx context.Context, in middleware.FinalizeInput, next middleware.FinalizeHandler) (out middleware.FinalizeOutput, metadata middleware.Metadata, err error) {
+			req, ok := in.Request.(*smithyhttp.Request)
+			if !ok {
+				return out, metadata, &v4.SigningError{Err: fmt.Errorf("(restoreIgnored) unexpected request middleware type %T", in.Request)}
+			}
+
+			ignored, _ := middleware.GetStackValue(ctx, ignoredHeadersKey{}).(map[string]string)
+			for k, v := range ignored {
+				req.Header.Set(k, v)
+			}
+
+			return next.HandleFinalize(ctx, in)
+		},
+	)
+}
 
 func extractRegionFromEndpointURL(endpointURL string) string {
 	re := regexp.MustCompile(`s3[.-](?P<region>[^.]+)\.`)
@@ -55,14 +160,31 @@ func (r *ResolveHandler) Handle(ctx context.Context, desc ocispec.Descriptor) (r
 		log.G(ctx).Error("S3_ENDPOINT_URL, AWS_ACCESS_KEY, and AWS_SECRET_ACCESS_KEY must be set")
 	}
 
-	log.G(ctx).Infof("s3 endpoint: %s", s3EndpointURL)
+	endpointURL, err := parseEndpoint(s3EndpointURL)
+	if err != nil {
+		return nil, 0, err
+	}
 
-	region := extractRegionFromEndpointURL(s3EndpointURL)
+	// use virtual-host-style if the endpoint is known to support it,
+	// otherwise use the path-style approach.
+	isVirtualHostStyle := isVirtualHostStyle(endpointURL)
+
+	useAccelerate := supportsTransferAcceleration(endpointURL)
+	// AWS SDK handles transfer acceleration automatically. Setting the
+	// Endpoint to a transfer acceleration endpoint would cause bucket
+	// operations fail.
+	if useAccelerate {
+		endpointURL = sentinelURL
+	}
+
+	log.G(ctx).Infof("s3 endpoint: %s", endpointURL.String())
+
+	region := extractRegionFromEndpointURL(endpointURL.String())
 
 	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 		return aws.Endpoint{
 			PartitionID:       "aws",
-			URL:               s3EndpointURL,
+			URL:               endpointURL.String(),
 			SigningRegion:     region,
 			HostnameImmutable: true,
 		}, nil
@@ -74,7 +196,15 @@ func (r *ResolveHandler) Handle(ctx context.Context, desc ocispec.Descriptor) (r
 		EndpointResolverWithOptions: resolver,
 	}
 
-	client := s3.NewFromConfig(cfg)
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = !isVirtualHostStyle
+		o.UseAccelerate = useAccelerate
+		// Google Cloud Storage alters the Accept-Encoding header, which breaks the v2 request signature
+		// (https://github.com/aws/aws-sdk-go-v2/issues/1816)
+		if IsGoogleEndpoint(endpointURL) {
+			ignoreSigningHeaders(o, []string{"Accept-Encoding"})
+		}
+	})
 	bucket := desc.Annotations[common.DescriptorAnnotationBucket]
 	objectKey := desc.Annotations[common.DescriptorAnnotationObjectKey]
 	size, err := statS3ObjectSize(ctx, client, bucket, objectKey)
