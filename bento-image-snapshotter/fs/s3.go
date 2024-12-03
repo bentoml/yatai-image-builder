edc25c47127b78ef4dd37c4767cff15ae4af2161
yetone <yetoneful@gmail.com>
2024-12-03 15:24:03 +0800

    feat: use presigned url to concurrently download image layers

--- bento-image-snapshotter/fs/s3.go
+++ bento-image-snapshotter/fs/s3.go
@@ -5,27 +5,38 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/containerd/log"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/bentoml/yatai-image-builder/common"
 )
 
 type S3FileSystem struct {
 	EnableRamfs bool
+	client      *s3.Client
 }
 
-func NewS3FileSystem(enableRamfs bool) S3FileSystem {
+func NewS3FileSystem(ctx context.Context, enableRamfs bool) (S3FileSystem, error) {
+	client, err := GetS3Client(ctx, true)
+	if err != nil {
+		log.G(ctx).WithError(err).Fatal("failed to create s3 client")
+		return S3FileSystem{}, errors.Wrap(err, "failed to create s3 client")
+	}
 	return S3FileSystem{
 		EnableRamfs: enableRamfs,
-	}
+		client:      client,
+	}, nil
 }
 
 func chownRecursive(ctx context.Context, root string, uid, gid int) error {
@@ -208,10 +219,138 @@ func stringifyCmd(cmd *exec.Cmd) string {
 	return cmdStr
 }
 
+func downloadRange(ctx context.Context, presignedURL string, start, end int64, writer io.WriterAt) error {
+	// use curl
+	logger := log.G(ctx).WithField("range", fmt.Sprintf("%d-%d", start, end)).WithField("url", presignedURL)
+	logger.Debug("downloading range...")
+	pr, pw := io.Pipe()
+	go func() {
+		var stderr bytes.Buffer
+		defer pw.Close()
+		cmd := exec.CommandContext(ctx, "curl", "--silent", "--show-error", "--fail", "--output", "-", "--range", fmt.Sprintf("%d-%d", start, end), presignedURL) // nolint:gosec
+		cmd.Stderr = &stderr
+		cmd.Stdout = pw
+		if err := cmd.Run(); err != nil {
+			pw.CloseWithError(errors.Wrapf(err, "failed to run command: %s, stderr: %s", stringifyCmd(cmd), stderr.String()))
+		}
+	}()
+	buf := make([]byte, end-start+1)
+	for {
+		n, err := pr.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return errors.Wrap(err, "failed to read from pipe")
+		}
+		_, err = writer.WriteAt(buf[:n], start)
+		if err != nil {
+			return errors.Wrap(err, "failed to write to writer")
+		}
+		start += int64(n)
+	}
+	logger.Debug("range downloaded")
+	return nil
+}
+
+func parrallelDownload(ctx context.Context, presignedURL string, destPath string, parallelism int) error {
+	// use curl to get the size of the file
+	logger := log.G(ctx).WithField("url", presignedURL)
+	logger.Debug("getting the size of the file...")
+	var stderr bytes.Buffer
+	cmd := exec.CommandContext(ctx, "curl", "--silent", "--show-error", "--fail", "--head", "--output", "-", presignedURL) // nolint:gosec
+	cmd.Stderr = &stderr
+	output, err := cmd.Output()
+	if err != nil {
+		return errors.Wrapf(err, "failed to run command: %s, stderr: %s", stringifyCmd(cmd), stderr.String())
+	}
+	var size int64
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines {
+		k, v, found := strings.Cut(line, ":")
+		if !found {
+			continue
+		}
+		if strings.ToLower(strings.TrimSpace(k)) == "content-length" {
+			var err error
+			size, err = strconv.ParseInt(strings.TrimSpace(v), 10, 64)
+			if err != nil {
+				return errors.Wrapf(err, "failed to parse size: %s", string(output))
+			}
+			logger.Debugf("size: %d", size)
+			break
+		}
+	}
+
+	if size == 0 {
+		return errors.New("cannot get the file size from presigned url")
+	}
+
+	partSize := size / int64(parallelism)
+	if size%int64(parallelism) > 0 {
+		partSize += 1
+	}
+
+	baseDir := filepath.Dir(destPath)
+	if err := os.MkdirAll(baseDir, 0755); err != nil {
+		return errors.Wrap(err, "failed to create base dir")
+	}
+
+	// create the target file and falloc the target file
+	file, err := os.Create(destPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to create target file")
+	}
+	defer file.Close()
+
+	// use truncate to fallocate the target file
+	// seek to the first byte of the file
+	if _, err := file.Seek(0, 0); err != nil {
+		return errors.Wrap(err, "failed to seek the target file")
+	}
+	// truncate the file to the size
+	if err := file.Truncate(size); err != nil {
+		return errors.Wrap(err, "failed to truncate the target file")
+	}
+	//
+	// if err := syscall.Fallocate(int(file.Fd()), 0, 0, size); err != nil {
+	// 	return errors.Wrap(err, "failed to fallocate the target file")
+	// }
+
+	var eg errgroup.Group
+	for i := 0; i < parallelism; i++ {
+		start := int64(i) * partSize
+		end := start + partSize - 1
+		if end >= size {
+			end = size - 1
+		}
+		eg.Go(func() error {
+			defer func() {
+				if err := recover(); err != nil {
+					if err_, ok := err.(error); ok {
+						log.G(ctx).WithError(err_).Error("recovered from panic")
+					}
+				}
+			}()
+			return downloadRange(ctx, presignedURL, start, end, file)
+		})
+	}
+	return errors.Wrap(eg.Wait(), "downloading")
+}
+
 func (o *S3FileSystem) downloadLayerFromS3(ctx context.Context, bucketName, layerKey string, destinationPath string) error {
 	baseName := filepath.Base(destinationPath)
 	dirName := filepath.Dir(destinationPath)
+	downloadedFilePath := filepath.Join(dirName, "downloads", layerKey+".tar.zst")
 	tempName := filepath.Join(dirName, baseName+".tmp")
+
+	defer func() {
+		if err := os.Remove(downloadedFilePath); err != nil {
+			if !os.IsNotExist(err) {
+				log.G(ctx).WithError(err).Error("failed to remove downloaded file")
+			}
+		}
+	}()
 
 	if o.EnableRamfs {
 		tempName = destinationPath
@@ -222,24 +361,39 @@ func (o *S3FileSystem) downloadLayerFromS3(ctx context.Context, bucketName, laye
 	}
 
 	logger := log.G(ctx).WithField("key", layerKey).WithField("destinationPath", tempName)
-	logger.Info("downloading and streaming decompression of the layer from S3...")
 
-	s3Path := fmt.Sprintf("s3://%s/%s", bucketName, layerKey)
+	presignedURL, err := GetS3PresignedURL(ctx, o.client, bucketName, layerKey)
+	if err != nil {
+		return errors.Wrap(err, "failed to get s3 presigned url")
+	}
+
+	coreNumber := runtime.NumCPU()
 
 	startTime := time.Now()
+	logger.Info("downloading layer from S3...")
+	err = parrallelDownload(ctx, presignedURL, downloadedFilePath, coreNumber)
+	if err != nil {
+		return errors.Wrap(err, "failed to download layer from S3")
+	}
+
+	downloadDuration := time.Since(startTime)
+	logger.WithField("duration", downloadDuration).Info("the layer has been downloaded from S3")
+
+	startTime = time.Now()
+	logger.Info("decompressing layer...")
 	var stderr bytes.Buffer
-	cmd := exec.CommandContext(ctx, "sh", "-c", fmt.Sprintf("s5cmd cat --concurrency=12 --part-size=100 %s | pzstd -d | tar -xf -", s3Path)) // nolint:gosec
+	cmd := exec.CommandContext(ctx, "sh", "-c", fmt.Sprintf("pzstd -d -c %s | tar -xf -", downloadedFilePath)) // nolint:gosec
 	cmd.Stderr = &stderr
 	cmd.Dir = tempName
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return errors.Wrapf(err, "failed to run command: %s, stderr: %s", stringifyCmd(cmd), stderr.String())
 	}
 
-	duration := time.Since(startTime)
+	decompressionDuration := time.Since(startTime)
 
-	logger.WithField("duration", duration).Info("the layer has been downloaded and decompressed")
+	logger.WithField("duration", decompressionDuration).Info("the layer has been decompressed")
 
 	if !o.EnableRamfs {
 		err = os.RemoveAll(destinationPath)
--- bento-image-snapshotter/fs/stargzs3/resolvehandler.go
+++ bento-image-snapshotter/fs/stargzs3/resolvehandler.go
@@ -6,208 +6,29 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
-	"os"
-	"regexp"
-
-	urlpkg "net/url"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/smithy-go/middleware"
 	"github.com/containerd/log"
 	"github.com/containerd/stargz-snapshotter/fs/remote"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 
-	smithyhttp "github.com/aws/smithy-go/transport/http"
+	"github.com/bentoml/yatai-image-builder/bento-image-snapshotter/fs"
 
 	"github.com/bentoml/yatai-image-builder/common"
 )
 
-var sentinelURL = urlpkg.URL{}
-
-const (
-	// Amazon Accelerated Transfer endpoint
-	transferAccelEndpoint = "s3-accelerate.amazonaws.com"
-
-	// Google Cloud Storage endpoint
-	gcsEndpoint = "storage.googleapis.com"
-)
-
-func supportsTransferAcceleration(endpoint urlpkg.URL) bool {
-	return endpoint.Hostname() == transferAccelEndpoint
-}
-
-func IsGoogleEndpoint(endpoint urlpkg.URL) bool {
-	return endpoint.Hostname() == gcsEndpoint
-}
-
-// isVirtualHostStyle reports whether the given endpoint supports S3 virtual
-// host style bucket name resolving. If a custom S3 API compatible endpoint is
-// given, resolve the bucketname from the URL path.
-func isVirtualHostStyle(endpoint urlpkg.URL) bool {
-	return endpoint == sentinelURL || supportsTransferAcceleration(endpoint) || IsGoogleEndpoint(endpoint)
-}
-
-func parseEndpoint(endpoint string) (urlpkg.URL, error) {
-	if endpoint == "" {
-		return sentinelURL, nil
-	}
-
-	u, err := urlpkg.Parse(endpoint)
-	if err != nil {
-		return sentinelURL, errors.Wrapf(err, "failed to parse endpoint: %s", endpoint)
-	}
-
-	return *u, nil
-}
-
-// ignoreSigningHeaders excludes the listed headers
-// from the request signature because some providers may alter them.
-//
-// See https://github.com/aws/aws-sdk-go-v2/issues/1816.
-func ignoreSigningHeaders(o *s3.Options, headers []string) {
-	o.APIOptions = append(o.APIOptions, func(stack *middleware.Stack) error {
-		if err := stack.Finalize.Insert(ignoreHeaders(headers), "Signing", middleware.Before); err != nil {
-			return errors.Wrap(err, "failed to insert ignoreHeaders middleware")
-		}
-
-		if err := stack.Finalize.Insert(restoreIgnored(), "Signing", middleware.After); err != nil {
-			return errors.Wrap(err, "failed to insert restoreIgnored middleware")
-		}
-
-		return nil
-	})
-}
-
-type ignoredHeadersKey struct{}
-
-func ignoreHeaders(headers []string) middleware.FinalizeMiddleware {
-	return middleware.FinalizeMiddlewareFunc(
-		"IgnoreHeaders",
-		func(ctx context.Context, in middleware.FinalizeInput, next middleware.FinalizeHandler) (out middleware.FinalizeOutput, metadata middleware.Metadata, err error) {
-			req, ok := in.Request.(*smithyhttp.Request)
-			if !ok {
-				return out, metadata, &v4.SigningError{Err: fmt.Errorf("(ignoreHeaders) unexpected request middleware type %T", in.Request)}
-			}
-
-			ignored := make(map[string]string, len(headers))
-			for _, h := range headers {
-				ignored[h] = req.Header.Get(h)
-				req.Header.Del(h)
-			}
-
-			ctx = middleware.WithStackValue(ctx, ignoredHeadersKey{}, ignored)
-
-			return next.HandleFinalize(ctx, in)
-		},
-	)
-}
-
-func restoreIgnored() middleware.FinalizeMiddleware {
-	return middleware.FinalizeMiddlewareFunc(
-		"RestoreIgnored",
-		func(ctx context.Context, in middleware.FinalizeInput, next middleware.FinalizeHandler) (out middleware.FinalizeOutput, metadata middleware.Metadata, err error) {
-			req, ok := in.Request.(*smithyhttp.Request)
-			if !ok {
-				return out, metadata, &v4.SigningError{Err: fmt.Errorf("(restoreIgnored) unexpected request middleware type %T", in.Request)}
-			}
-
-			ignored, _ := middleware.GetStackValue(ctx, ignoredHeadersKey{}).(map[string]string)
-			for k, v := range ignored {
-				req.Header.Set(k, v)
-			}
-
-			return next.HandleFinalize(ctx, in)
-		},
-	)
-}
-
-func extractRegionFromEndpointURL(endpointURL string) string {
-	re := regexp.MustCompile(`s3[.-](?P<region>[^.]+)\.`)
-	match := re.FindStringSubmatch(endpointURL)
-	if len(match) > 1 {
-		return match[1]
-	}
-	return "us-east-1"
-}
-
-func statS3ObjectSize(ctx context.Context, client *s3.Client, bucket, objectKey string) (int64, error) {
-	log.G(ctx).Infof("statting %s/%s", bucket, objectKey)
-	output, err := client.HeadObject(ctx, &s3.HeadObjectInput{
-		Bucket: &bucket,
-		Key:    &objectKey,
-	})
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to head object")
-	}
-	if output.ContentLength == nil {
-		return 0, errors.New("content length is nil")
-	}
-	return *output.ContentLength, nil
-}
-
 type ResolveHandler struct{}
 
 func (r *ResolveHandler) Handle(ctx context.Context, desc ocispec.Descriptor) (remote.Fetcher, int64, error) {
-	s3EndpointURL := os.Getenv("S3_ENDPOINT_URL")
-	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
-	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-
-	if s3EndpointURL == "" || accessKey == "" || secretKey == "" {
-		log.G(ctx).Error("S3_ENDPOINT_URL, AWS_ACCESS_KEY, and AWS_SECRET_ACCESS_KEY must be set")
-	}
-
-	endpointURL, err := parseEndpoint(s3EndpointURL)
+	client, err := fs.GetS3Client(ctx, false)
 	if err != nil {
-		return nil, 0, err
+		return nil, 0, errors.Wrap(err, "failed to get s3 client")
 	}
-
-	// use virtual-host-style if the endpoint is known to support it,
-	// otherwise use the path-style approach.
-	isVirtualHostStyle := isVirtualHostStyle(endpointURL)
-
-	useAccelerate := supportsTransferAcceleration(endpointURL)
-	// AWS SDK handles transfer acceleration automatically. Setting the
-	// Endpoint to a transfer acceleration endpoint would cause bucket
-	// operations fail.
-	if useAccelerate {
-		endpointURL = sentinelURL
-	}
-
-	log.G(ctx).Infof("s3 endpoint: %s", endpointURL.String())
-
-	region := extractRegionFromEndpointURL(endpointURL.String())
-
-	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		return aws.Endpoint{
-			PartitionID:       "aws",
-			URL:               endpointURL.String(),
-			SigningRegion:     region,
-			HostnameImmutable: true,
-		}, nil
-	})
-
-	cfg := aws.Config{
-		Region:                      region,
-		Credentials:                 credentials.NewStaticCredentialsProvider(accessKey, secretKey, ""),
-		EndpointResolverWithOptions: resolver,
-	}
-
-	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
-		o.UsePathStyle = !isVirtualHostStyle
-		o.UseAccelerate = useAccelerate
-		// Google Cloud Storage alters the Accept-Encoding header, which breaks the v2 request signature
-		// (https://github.com/aws/aws-sdk-go-v2/issues/1816)
-		if IsGoogleEndpoint(endpointURL) {
-			ignoreSigningHeaders(o, []string{"Accept-Encoding"})
-		}
-	})
 	bucket := desc.Annotations[common.DescriptorAnnotationBucket]
 	objectKey := desc.Annotations[common.DescriptorAnnotationObjectKey]
-	size, err := statS3ObjectSize(ctx, client, bucket, objectKey)
+	size, err := fs.StatS3ObjectSize(ctx, client, bucket, objectKey)
 	if err != nil {
 		log.G(ctx).Errorf("failed to stat %s/%s: %v", bucket, objectKey, err)
 		return nil, 0, nil
@@ -245,8 +66,8 @@ func (f *fetcher) Fetch(ctx context.Context, off int64, size int64) (io.ReadClos
 }
 
 func (f *fetcher) Check() error {
-	_, err := statS3ObjectSize(context.Background(), f.client, f.bucket, f.objectKey)
-	return err
+	_, err := fs.StatS3ObjectSize(context.Background(), f.client, f.bucket, f.objectKey)
+	return errors.Wrapf(err, "failed to stat %s/%s", f.bucket, f.objectKey)
 }
 
 func (f *fetcher) GenID(off int64, size int64) string {
