41c03730f84ebe9a117da1cc3e8fbc7bbbb52555
Frost Ming <me@frostming.com>
2024-12-18 13:53:32 +0800

    fix: update to 0.0.10
    Signed-off-by: Frost Ming <me@frostming.com>

--- controllers/resources/bentorequest_controller.go
+++ controllers/resources/bentorequest_controller.go
@@ -3097,7 +3097,7 @@ echo "Done"
 	cmd := shquot.POSIXShell(append(command, args...))
 
 	if imageStoredInS3 {
-		builderImage = "quay.io/bentoml/bento-image-builder:0.0.9"
+		builderImage = "quay.io/bentoml/bento-image-builder:0.0.10"
 		extraFlags := ""
 		for _, buildArg := range buildArgs {
 			extraFlags = fmt.Sprintf("%s --build-arg %s", extraFlags, strings.Replace(buildArg, "=", ":", 1))
