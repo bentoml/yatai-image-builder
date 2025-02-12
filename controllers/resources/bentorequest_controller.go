0ad5fb78d07f37b4d409928de1be752f4417e90a
yetone <yetoneful@gmail.com>
2025-02-12 14:13:27 +0800

    fix: checkS3ObjectExistsUsingAWSSDK (#185)

--- controllers/resources/bentorequest_controller.go
+++ controllers/resources/bentorequest_controller.go
@@ -3102,7 +3102,7 @@ echo "Done"
 	}
 
 	if imageStoredInS3 {
-		builderImage = "quay.io/bentoml/bento-image-builder:0.1.0"
+		builderImage = "quay.io/bentoml/bento-image-builder:0.1.1"
 		extraFlags := ""
 		for _, buildArg := range buildArgs {
 			quotedBuildArg := unix.SingleQuote.Quote(buildArg)
