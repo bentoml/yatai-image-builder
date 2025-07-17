a0838035bf048696399d21f40dcda969893ae262
xianml <xian@bentoml.com>
2025-07-17 18:16:19 +0800

    chore: upgrade bento-image-builder

--- controllers/resources/bentorequest_controller.go
+++ controllers/resources/bentorequest_controller.go
@@ -3119,7 +3119,7 @@ echo "Done"
 	}
 
 	if imageStoredInS3 {
-		builderImage = "quay.io/bentoml/bento-image-builder:0.1.1"
+		builderImage = "quay.io/bentoml/bento-image-builder:0.1.3"
 		extraFlags := ""
 		for _, buildArg := range buildArgs {
 			quotedBuildArg := unix.SingleQuote.Quote(buildArg)
