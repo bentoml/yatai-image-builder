cbb41e4ecd76c8b237fe4ed0b7746b294f0a51cd
xianxian.zhang <141706136+xianml@users.noreply.github.com>
2025-08-20 13:23:12 +0800

    chore: upgrade bento-image-builder (#219)
    * fix: zero size
    
    * fix: new builder version
    
    * chore: update image version
    
    * chore: upgrade version to 0.1.8

--- controllers/resources/bentorequest_controller.go
+++ controllers/resources/bentorequest_controller.go
@@ -3119,7 +3119,7 @@ echo "Done"
 	}
 
 	if imageStoredInS3 {
-		builderImage = "quay.io/bentoml/bento-image-builder:0.1.3"
+		builderImage = "quay.io/bentoml/bento-image-builder:0.1.8"
 		extraFlags := ""
 		for _, buildArg := range buildArgs {
 			quotedBuildArg := unix.SingleQuote.Quote(buildArg)
